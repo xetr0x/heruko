fc810657aeb8c5d837660eb9b78ebc472b0f20bb
kevin wilusz <kwilusz10@gmail.com>
2021-09-27 13:26:13 +0200

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+// Set access to the fs (file system) module
+const fs = require('fs');
+
+// Create a local server that responds & sends data back to the requestor (browser)
+const server = http.createServer();
+
+// Declare a function for what happens when the (incoming) request event triggers
+server.on('request', (req, res) => {
+        fs.readFile('index.html',function (err, data){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.setHeader('Content-Length', data.length);
+            res.write(data);
+            res.end();
+        });
+    
+});
+
+// Adding a dynamic PORT for Heroku, locally still 8042
+const PORT = process.env.PORT || 8080;
+
+// Have the server start listening locally to the port 8042,
+// and with Heroku it will get a dynamic port assigned automatically
+server.listen(PORT);
+
+console.log(`Server up and listening on port ${PORT}`);
